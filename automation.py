1932c4e8affdfa0e4a1d19ee118f8205ad2d0843
vitkovit <78933205+vitkovit@users.noreply.github.com>
2021-02-16 04:32:01 +0100

    Naild it - needs cleanup

--- automation.py
+++ automation.py
@@ -1,12 +1,21 @@
 
 from netmiko import ConnectHandler
-
+import yaml
+import datetime
+  
+# datetime.datetime.now() to get  
+# current date as filename. 
+filename = datetime.datetime.now()
+x=filename.strftime("%d_%m_%Y_%H:%M:%S")
+print(x)
+"""
 iou01 = { 
 'device_type': 'cisco_ios', 
 'ip': '10.0.0.101',
 'username': 'cisco', 
 'password': 'cisco', 
 }
+print(type(iou01))
 
 device = ConnectHandler(**iou01)
 
@@ -14,4 +23,5 @@ output1 = device.send_command("show running-config")
 save_file = open("iou-core01.txt","w")
 save_file.write(output1)
 save_file.close()
-device.disconnect()
+device.disconnect()"""
+
--- collect_run_v02.py
+++ collect_run_v02.py
@@ -54,6 +54,13 @@ if __name__ == "__main__":
 
 from netmiko import ConnectHandler
 import yaml
+import datetime
+  
+
+filename = datetime.datetime.now()
+TIME=filename.strftime("%d_%m_%Y_%H-%M-%S")
+
+
 #with open("inventory_01.yml") as f:
 #    my_inventory=yaml.safe_load(f)
 #def output_ip():
@@ -64,18 +71,19 @@ my_inventory_yaml = yaml.safe_load(my_inventory)            # readed output save
 #print(my_inventory_yaml["all"])                              # this will print whole dictionary "all"
 #print(type(my_inventory_yaml["all"]["sites"]))              # this is a LIST in the DICT
 #print(my_inventory_yaml["all"]["sites"])                    # this will print list for "sites"
-list_all_ip = []                                             # create empty list where IP will be stored
-list_all_host = []
+#list_all_ip = []                                             # create empty list where IP will be stored
+#list_all_host = []
 #list_all_ip = {}                                               # create empty DICT
+#host_dict = {}
 for site_dict in my_inventory_yaml["all"]["sites"]:            # this will check dictionary under key "all" and list "sites"
     #print(type(site_dict["hosts"]))    # this is LIST
     #print(type(site_dict))             # this is DICT
     #print(site_dict["hosts"])    # this will print out list under "hosts"
     #print(site_dict)             # this will print out dictionary under "sites"
-    for host in site_dict["hosts"]:
+#    for host in site_dict["hosts"]:
     #print(type(host))               # this is DICT
         #print(host)                      # this will print out nested dictionary for every "host"
-        for device, device_data in host.items():
+#        for device, device_data in host.items():
             #print(type(device))        # this is STR
             #print(device)              # this print out key values in dictionary
             #print(type(device_data))   # this is nested DICT
@@ -83,20 +91,98 @@ for site_dict in my_inventory_yaml["all"]["sites"]:            # this will check
             #print(device_data["ip"])   # this will print out device data for key value "ip", this can be any other key value
             #print(type(device_data["ip"]))                 # this will be STR
             #print(device_data["ip"].split('/')[0])          # this will split str by '/' and print out position0
-            ip_list = device_data["ip"].split('/')[0]       # creating valiable to loak cleaner
-            list_all_ip.append(ip_list)                   # add every ip from the loop in the list_all_ip list
-            list_all_host.append(device)
+#            ip_list = device_data["ip"].split('/')[0]       # creating valiable to loak cleaner
+#            list_all_ip.append(ip_list)                   # add every ip from the loop in the list_all_ip list
+#            list_all_host.append(device)
             #list_all_ip.update(ip_list)
             #print(ip_list)
-             
+            
+    login_credentials = my_inventory_yaml["all"]["vars"]
+    #print(site_dict["hosts"])
+    #print(type(site_dict["hosts"]))
+    for host in site_dict["hosts"]:
+        #print(type(host))
+        host_dict = {}
+        host_dict.update({'device_type': 'cisco_ios'})
+        #host_dict.update(host)
+        host_dict.update(login_credentials)
+        #print(host_dict)
+        #print(host_dict['username'])
+        #print(host)
+        for key, value in host.items():
+            #print(type(value))
+            #print(key)
+            #print(type(key))
+            #ip_list= []
+            #ip_list.append(value['ip'].split('/')[0])
+            ip_list = value['ip'].split('/')[0]
+            #print(host_dict)                        # this will print dictionary device type, username, password
+            #x=ip_list
+            #return ip_list
+            #print(type(x))                                 # this will print list on IP addresses
+            #print(ip_list)
+            y=dict.fromkeys({'ip'})
+            y['ip']=ip_list
+            
+            #print(y)
+            #    print(q)
+            host_dict.update(y)
+            #site_dict.update(host_dict)
+            #print(host_dict)                           # final format - need to be assigned to new key
+            #print(type(host_dict))
+            
+            #print(site_dict)
+            #z = {}
+            #for key in host.keys():
+            #    key=dict(key + host_dict)
+            #    print(key)
+            for key in host:
+                q=[]
+                q.append(key)
+                #print(type(q))
+                name=key
+                #print(q)
+                #print(name)
+                x=dict.fromkeys(q,host_dict)
+                #print(type(x))
+                #print(len(x))
+                
+
+                device = ConnectHandler(**host_dict)
+                output = device.send_command("show running-config")
+                save_file = open("{}_{}.txt".format(name,TIME),"w")
+                save_file.write(output)
+                save_file.close()
+                device.disconnect()
+            
+
+            #print(ip_list)
+            #for y in host_dict:
+            #    y=dict.fromkeys()
+            #    host_dict.update(y)
+            #print(host_dict)
+            #host_dict.update(x)
+            #print(ip_list)
+            #host_dict[key].append(ip_list)
+            #x=value['ip'].split('/')[0]
+            #host_dict.update({'ip':value})
+            #host_dict.update(value['ip'],'a')
+            #host_dict.update(x)
+           # print(ip_list)
+            #print(host_dict)
+#print(host_dict)           
+# 'device_type': 'cisco_ios', 
+#'ip': '10.0.0.101',
+#'username': 'cisco', 
+#'password': 'cisco',      
 
 
 
 
 #x=zip(list_all_host, list_all_ip)                                        # this exit the loops and check what is in the new list
 #print(list_all_ip)
-x = dict(zip(list_all_host,list_all_ip))
-print(x)
+#x = dict(zip(list_all_host,list_all_ip))
+#print(x)
 #x=dict.fromkeys(list_all_host, list_all_ip)
 #print(x)
 #return(list_all_ip)                                        # return can be used if you use function
